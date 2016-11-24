0afc93d4ee4cce4ccacc72ce6a8d182963609cfc
Alexis Borovik <borovikalexey@mail.ru>
2016-11-24 23:53:05 +0300

    New project structure created

---
+++ ComiX/ComiX/Application/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ComiX
+//
+//  Created by Alexis on 11/24/16.
+//  Copyright © 2016 Alexis Borovik. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
