bd7999c8c7498d564697c88167c5c2ec0d59c89c
Alexis Borovik <borovikalexey@mail.ru>
2016-11-27 18:35:01 +0300

    Some fixes, add new models, add new pods

---
+++ ComiX/ComiX/Models/PageItemTransform.swift
@@ -0,0 +1,9 @@
+//
+//  PageItemTransform.swift
+//  ComiX
+//
+//  Created by Alexis on 11/27/16.
+//  Copyright © 2016 Alexis Borovik. All rights reserved.
+//
+
+import Foundation
---
+++ ComiX/ComiX/Models/PageTemplate.swift
@@ -0,0 +1,9 @@
+//
+//  PageTemplate.swift
+//  ComiX
+//
+//  Created by Alexis on 11/27/16.
+//  Copyright © 2016 Alexis Borovik. All rights reserved.
+//
+
+import Foundation
